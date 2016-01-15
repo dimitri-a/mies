353bca56843c78d9422f3e70797d094d3dc77de6
Dimitri Couwenberg <Dimitri.Couwenberg@iag.com.au>
2016-01-15 13:08:06 +1100

    added colleague view

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
-var app = angular.module('app',['ui.router']);
+var app = angular.module('app', ['ui.router']);
 
 
-app.config(function($stateProvider,$urlRouterProvider){
+app.config(function ($stateProvider, $urlRouterProvider) {
 
     console.log('inside the stateprovider');
 
@@ -10,24 +10,67 @@ app.config(function($stateProvider,$urlRouterProvider){
     $urlRouterProvider.otherwise('/home')
 
     $stateProvider
-        .state('testing',{
-            url:"/testing",
-            template:'<p>this is testing</p>'
+        .state('manager', {
+            url: "/manager",
+
+            templateUrl: "views/manager.html",
+
+            resolve: {
+                typeEmployee: function ($q, $timeout) {
+                    var deferred = $q.defer();
+                    $timeout(function () {
+                        deferred.resolve('manager');
+                    }, 200);
+                    return deferred.promise;
+                },
+
+                controller: function (typeEmployee) {
+                    console.log('type of employee is:', typeEmployee);
+                    if (typeEmployee === 'colleague') {
+                        console.log('not allowed to view this');
+                    }
+
+                    if (typeEmployee === 'manager') {
+                        console.log('allowed to view this');
+                    }
+
+                }
+            }
         })
 
-        .state('manager',{
-            url:"/manager",
-            templateUrl:'views/manager.html'
+        .state('colleague', {
+            url: "/colleague",
+
+            templateUrl: "views/manager.html",
+
+            resolve: {
+                typeEmployee: function ($q, $timeout) {
+                    var deferred = $q.defer();
+                    $timeout(function () {
+                        deferred.resolve('manager');
+                    }, 200);
+                    return deferred.promise;
+                },
+
+                controller: function (typeEmployee) {
+                    console.log('type of employee is:', typeEmployee);
+                    if (typeEmployee === 'colleague') {
+                        console.log('not allowed to view this');
+                    }
+
+                    if (typeEmployee === 'manager') {
+                        console.log('allowed to view this');
+                    }
+
+                }
+            }
         })
 
-        .state('home',{
-            url:"/"
+
+        .state('home', {
+            url: "/"
         })
 
-});
-
-app.run(function(){
-    console.log('inside run');
 });
 
 
