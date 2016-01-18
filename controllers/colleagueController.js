8bf4b2ed1cdf341280fc652d53fd9927f32670ec
Dimitri Couwenberg <Dimitri.Couwenberg@iag.com.au>
2016-01-18 17:08:29 +1100

    controller working

--- controllers/colleagueController.js
+++ controllers/colleagueController.js
@@ -3,35 +3,14 @@ function colCtrl($scope,msg) {
     console.log(msg);
     console.log('inside colctrl');
 
-    $scope.items =[
-        {'url':'http://www.google.com','text':'Add new exception'},
-        {'url':'http://www.google.com','text':'Contact WFP&O'},
-
-        {'url':'http://www.google.com','text':'Request Flex for today'},
-
-        {'url':'http://www.google.com','text':'My business units activities for today'},
-
-        {'url':'http://www.google.com','text':'See my business Wallboard'},
-
-
-
-
-        {'url':'http://www.google.com','text':'Amend my roster'}
-
+    $scope.items = [
+        {'url': 'http://www.google.com', 'text': 'Add new exception'},
+        {'url': 'http://www.google.com', 'text': 'Contact WFP&O'},
+        {'url': 'http://www.google.com', 'text': 'Request Flex for today'},
+        {'url': 'http://www.google.com', 'text': 'My business units activities for today'},
+        {'url': 'http://www.google.com', 'text': 'See my business Wallboard'},
+        {'url': 'http://www.google.com', 'text': 'Amend my roster'}
     ];
 }
-
-    //console.log('type of employee is:');
-    //
-    ////if (typeEmployee === 'colleague') {
-    ////    console.log('not allowed to view this');
-    ////};
-    ////
-    ////if (typeEmployee === 'manager') {
-    ////    console.log('allowed to view this');
-    ////};
-    //
-
-
 
 app.controller('colCtrl', colCtrl);
--- controllers/managerCtrl.js
+++ controllers/managerCtrl.js
@@ -1,30 +1,21 @@
-function managerCtrl($scope,msg){
+function managerCtrl($scope, msg) {
     console.log(msg);
 
     console.log('inside mgr ctrl');
 
-    $scope.items =[
-        {'url':'http://www.google.com','text':'Add new exception'},
-        {'url':'http://www.google.com','text':'Contact WFP&O'},
-        {'url':'http://www.google.com','text':'Request Flex for today'},
-        {'url':'http://www.google.com','text':'My business units activities for today'},
-        {'url':'http://www.google.com','text':'See my business Wallboard'},
-
-        {'url':'http://www.google.com','text':'Intranet Updates'},
-
-
-        {'url':'http://www.google.com','text':'NSM'},
-        {'url':'http://www.google.com','text':'PI MART'},
-
-        {'url':'http://www.google.com','text':'Skills matrix'},
-
-        {'url':'http://www.google.com','text':'Forms'},
-
-        {'url':'http://www.google.com','text':'WFP&O Processes & Links'},
-
-        {'url':'http://www.google.com','text':'My business Units Activities For Today'},
-
-
-        {'url':'http://www.google.com','text':'Amend my roster'}
+    $scope.items = [
+        {'url': 'http://www.google.com', 'text': 'Add new exception'},
+        {'url': 'http://www.google.com', 'text': 'Contact WFP&O'},
+        {'url': 'http://www.google.com', 'text': 'Request Flex for today'},
+        {'url': 'http://www.google.com', 'text': 'My business units activities for today'},
+        {'url': 'http://www.google.com', 'text': 'See my business Wallboard'},
+        {'url': 'http://www.google.com', 'text': 'Intranet Updates'},
+        {'url': 'http://www.google.com', 'text': 'NSM'},
+        {'url': 'http://www.google.com', 'text': 'PI MART'},
+        {'url': 'http://www.google.com', 'text': 'Skills matrix'},
+        {'url': 'http://www.google.com', 'text': 'Forms'},
+        {'url': 'http://www.google.com', 'text': 'WFP&O Processes & Links'},
+        {'url': 'http://www.google.com', 'text': 'My business Units Activities For Today'},
+        {'url': 'http://www.google.com', 'text': 'Amend my roster'}
     ];
 };
