c5601805b97870fe62632e914dd16887c59c6c32
Dimitri Couwenberg <Dimitri.Couwenberg@iag.com.au>
2016-01-19 13:18:02 +1100

    gulp is working

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 var gulp = require('gulp'),
     connect = require('gulp-connect');
 
+var open =require('open');
+
 gulp.task('connect', function() {
     connect.server();
 });
 
-gulp.task('default', ['connect']);
+var watch = require('gulp-watch');
+
+gulp.task('watch', function() {
+    gulp.watch('*.js')
+    .pipe(connect.reload());
+});
+
+gulp.task('open', function(){
+    gulp.src('index.html')
+        .pipe(open());
+});
+
+
+gulp.task('default', ['connect','open']);
+
